98c41dccd14466bb5fe41dc4b4fcf19ecd80b74c
Jovan Lanik <jox969@gmail.com>
2019-01-25 19:22:31 +0100

    Moved pages to ./pages/

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="refresh" content="0; URL='pages/home.html'" />
+		<title>Bakalab</title> 
+		<script>window.location = "pages/home.html";</script>
+	</head>
+	<body>
+		<?php
+			header("Location: pages/home.html");
+			exit();
+		?>
+	</body>
+</html>
