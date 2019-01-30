f9dd5266ee710ccb20088797c87460117245a96e
Josef Miegl <josef@miegl.cz>
2019-01-30 20:17:38 +0100

    fix code style

--- scripts/downloadLatest.js
+++ scripts/downloadLatest.js
@@ -1,8 +1,8 @@
 function downloadLatest()
 {
 	var req = new XMLHttpRequest();
-    req.open("GET", "https://api.github.com/repos/bakalaborg/bakalab/releases",false);
-    req.send(null);
-    var releases = JSON.parse(req.responseText);
+	req.open("GET", "https://api.github.com/repos/bakalaborg/bakalab/releases",false);
+	req.send(null);
+	var releases = JSON.parse(req.responseText);
 	window.location.assign(releases[0].assets[0].browser_download_url);
 }
